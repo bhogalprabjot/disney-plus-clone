b4140114ff727c70e6b5abc15b0c266019b1f3c7
Prabjot Singh Bhogal <bhogalprabjot1@gmail.com>
2021-12-12 23:29:02 +0530

    details navigation added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 import './App.css';
+import Details from './components/Details';
 import Header from './components/Header';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Header />
-      <Home />
+        <Routes>
+          <Route path="/detail" element={<Details/>}></Route>
+          <Route path="/" element={<Home/>}></Route>
+        </Routes>
+      </Router>
 
     </div>
   );
